fd32fa1df51cc1921031b4e967a239027ffe5ae5
Elad Yitzhari <eladyzhari@gmail.com>
2019-12-05 14:24:43 +0200

    Updates

---
+++ precache-manifest.69a5f5a3c8cc50b049bb76a81ebf0897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e6dbca2163a5c76bb4e69bb3e085a9",
+    "url": "/AnchorReact/index.html"
+  },
+  {
+    "revision": "71767d123cdd8eb9f6b9",
+    "url": "/AnchorReact/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "5a986f2da102753d8b2b",
+    "url": "/AnchorReact/static/css/main.d17afec1.chunk.css"
+  },
+  {
+    "revision": "71767d123cdd8eb9f6b9",
+    "url": "/AnchorReact/static/js/2.0aa8e0b4.chunk.js"
+  },
+  {
+    "revision": "5a986f2da102753d8b2b",
+    "url": "/AnchorReact/static/js/main.77d90306.chunk.js"
+  },
+  {
+    "revision": "74dc33019ea9fb7a2789",
+    "url": "/AnchorReact/static/js/runtime-main.a60175b8.js"
+  }
+]);
