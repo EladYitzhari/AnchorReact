770f1dbe3434126eb21de7bae5cf90a798a6e861
Elad Yitzhari <eladyzhari@gmail.com>
2020-02-06 12:44:05 +0200

    upload in parallel to the server upload in git

--- src/components/Functions/location.js
+++ src/components/Functions/location.js
@@ -1,4 +1,4 @@
 export const serverAdress=()=>{
-    // return "https://env-4171164.j.box.co.il/AnchorOpen/";
-    return "http://localhost:8080/";
+    return "https://env-4171164.j.box.co.il/AnchorOpen/";
+    // return "http://localhost:8080/";
 }
